745ffcc0aa175052f0f036d029d923d2e0b22342
xxs <1085465699@qq.com>
2020-11-30 14:41:47 +0800

    vue移动端模板 1.0

--- src/server/http.js
+++ src/server/http.js
@@ -13,20 +13,17 @@ const showToast = msg => {
         });
     }
     /**
-     * 
-     * @param {*} url api的key值
-     * @param {*} method 请求方法 get post
-     * @param {*} data 请求参数 get与post写法一致
-     * @param {*} headers 请求头 非必填
-     * @param {*} msg toast提示信息 非必填 
+     * @param {string} url api的key值
+     * @param {object} data 请求参数 get与post写法一致
+     * @param {string} msg toast提示信息 非必填 
+     * @param {string} method 请求方法 get post 默认post非必填
+     * @param {string} headers 请求头 非必填
      */
 export default function http({ url, data, msg, method = 'post', headers = "application/x-www-form-urlencoded" }) {
     msg ? showToast(msg) : showToast('请求中,请稍候')
         // application/x-www-form-urlencoded  默认格式
         // application/json;charset=UTF-8  
     return new Promise((resolve, reject) => {
-
-        console.log(data)
         var httpUrl = api[url]
         if (method === 'get') {
             httpUrl += '?'
@@ -34,7 +31,6 @@ export default function http({ url, data, msg, method = 'post', headers = "appli
                 httpUrl += key + '=' + data[key]
             }
         }
-
         axios({
                 headers: { "Content-Type": headers },
                 url: httpUrl,
@@ -42,11 +38,11 @@ export default function http({ url, data, msg, method = 'post', headers = "appli
                 data: method === 'get' ? {} : data
             }).then(res => {
                 Toast.clear();
+                // 根据实际后端返回去判断
                 if (res.data.code === 0) {
                     resolve(res.data.data)
                 } else {
                     Toast.fail(res.data.msg || '请求错误！');
-
                     reject(res.data.msg || '请求错误！')
                 }
             })
@@ -54,11 +50,9 @@ export default function http({ url, data, msg, method = 'post', headers = "appli
                 Toast.clear();
                 if (typeof err === 'string') {
                     Toast.fail(err);
-
                     reject(err)
                 } else {
                     Toast.fail(JSON.stringify(err));
-
                     reject(JSON.stringify(err))
                 }
             })
